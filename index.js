2c7c3d2922ee69a776d65ad82bff207174b61d28
Kerry Gilbert <kerry@punkave.com>
2013-04-03 15:29:15 -0400

    set up package.json and index.js. made a parseJSON function and a getBus function that uses parseJSON to hit septa's api

---
+++ index.js
@@ -0,0 +1,52 @@
+var http = require('http');
+
+module.exports = septa;
+
+function septa(app) {
+  return new septa.Septa(app);
+}
+
+septa.Septa = function(app) {
+  var self = this;
+
+  /* Makes an http request and attempts to turn the response data into a JSON object */
+  self.getJSON = function(options, callback) {
+    var req = http.request(options, function(res) {
+      var output = '';
+      res.setEncoding('utf8');
+
+      res.on('data', function (data) {
+        output += data;
+      });
+
+      res.on('end', function() {
+        var obj = JSON.parse(output);
+        callback(obj);
+      });
+    });
+
+    req.on('error', function(err) {
+      res.send('error: ' + err.message);
+    });
+
+    req.end();
+  };  
+
+  /* A sample function that builds out an options object with info pertinent to SEPTA's API,
+  calls getJSON and then  */ 
+  self.getBus = function(route, callback) {
+    var options = {
+      host: 'www3.septa.org',
+      path:'/hackathon/TransitView/'+route,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    self.getJSON(options, function(data) {
+      console.log(data)
+      callback(data);
+    }); 
+  }
+}
